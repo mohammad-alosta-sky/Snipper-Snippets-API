3f38f60ce9e44a209f0089b9605ecf67019ae63f
Mohammad Al osta <mohammad.alosta@sky.uk>
2024-01-12 13:55:55 +0000

    initial commit

---
+++ src/main/java/com/multiverse/SnipperSnippetsAPI/SnipperSnippetsApiApplication.java
@@ -0,0 +1,13 @@
+package com.multiverse.SnipperSnippetsAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnipperSnippetsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnipperSnippetsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/multiverse/SnipperSnippetsAPI/SnipperSnippetsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multiverse.SnipperSnippetsAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnipperSnippetsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
